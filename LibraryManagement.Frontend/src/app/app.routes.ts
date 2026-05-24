28cefea045f772b9980581440a9720a6dae0b471
JUAN MANUEL COLORADO NAVAS <juancolorado316345@correo.itm.edu.co>
2026-05-24 12:26:21 -0500

    feat: configure cors and angular frontend base

--- LibraryManagement.Frontend/src/app/app.routes.ts
+++ LibraryManagement.Frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { CategoryList } from './pages/categories/category-list/category-list';
+import { AuthorList } from './pages/authors/author-list/author-list';
+import { MemberList } from './pages/members/member-list/member-list';
+import { BookList } from './pages/books/book-list/book-list';
+import { LoanList } from './pages/loans/loan-list/loan-list';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'categories',
+    component: CategoryList
+  },
+  {
+    path: 'authors',
+    component: AuthorList
+  },
+  {
+    path: 'members',
+    component: MemberList
+  },
+  {
+    path: 'books',
+    component: BookList
+  },
+  {
+    path: 'loans',
+    component: LoanList
+  },
+  {
+    path: '**',
+    component: NotFound
+  }
+];
